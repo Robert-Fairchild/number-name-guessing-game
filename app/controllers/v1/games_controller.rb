0ec48362c51fc9fc22b295abdd1c1700ee223c11
Robert Fairchild <robertjoseph.fairchild@gmail.com>
2017-11-15 19:55:37 -0600

    new number method

--- app/controllers/v1/games_controller.rb
+++ app/controllers/v1/games_controller.rb
@@ -2,14 +2,17 @@ class V1::GamesController < ApplicationController
   def query_params_method
     first_name = params["f_name"]
     last_name = params["l_name"]
-    number = params["num"].to_i
-
-    # if first_name.start_with?("R") 
-    #   render json: {message: "Your name starts with an R, congratulations"}
-    # else
-    #   render json: {message:" Your name does not start with an R"}
-    # end 
     
+
+    if first_name.start_with?("R") 
+      render json: {message: "Your name starts with an R, congratulations"}
+    else
+      render json: {message:" Your name does not start with an R"}
+    end 
+  end 
+    
+  def number_params_method
+    number = params["num"].to_i
     if number < 45 
       render json: {message: "guess higher home skillet"}
     elsif number > 45 
@@ -17,7 +20,9 @@ class V1::GamesController < ApplicationController
     elsif number == 45 
       render json: {message: "You win bro!"}
     end 
-
-
   end 
+
+
+
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   namespace :v1 do 
     get "/query_params" => "games#query_params_method"
+    get "/number_params" => "games#number_params_method"
   end
 end
