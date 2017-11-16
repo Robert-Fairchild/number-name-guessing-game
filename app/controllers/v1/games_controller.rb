33a605b17d2b2887675316e4bbad9b31e6936618
Robert Fairchild <robertjoseph.fairchild@gmail.com>
2017-11-15 19:37:43 -0600

    Message fix

--- app/controllers/v1/games_controller.rb
+++ app/controllers/v1/games_controller.rb
@@ -5,16 +5,17 @@ class V1::GamesController < ApplicationController
     number = params["num"].to_i
 
     # if first_name.start_with?("R") 
-    #   render json: {message: "Your name starts with an R bitch"}
+    #   render json: {message: "Your name starts with an R, congratulations"}
     # else
-    #   render json: {message:" Go fuck yourself"}
+    #   render json: {message:" Your name does not start with an R"}
     # end 
+    
     if number < 45 
       render json: {message: "guess higher home skillet"}
     elsif number > 45 
-      render json: {message: "Lower mang"}
+      render json: {message: "guess lower man"}
     elsif number == 45 
-      render json: {message: "You win Peter, you lying sack of horse shit!"}
+      render json: {message: "You win bro!"}
     end 
 
 
